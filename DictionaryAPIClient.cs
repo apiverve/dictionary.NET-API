6d4a91b0a237bd9b0f7434473b5300d70ad88546
APIVerve Team <hello@apiverve.com>
2024-08-20 10:48:52 -0500

    Bug Fixes

--- DictionaryAPIClient.cs
+++ DictionaryAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, dictionaryQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, DictionaryQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(dictionaryQueryOptions options = null)
+        public ResponseObj Execute(DictionaryQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(dictionaryQueryOptions options)
+        private string constructURL(DictionaryQueryOptions options)
         {
             string url = _apiEndpoint;
 
